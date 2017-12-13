1801ae15b461cd6cb60cfd867e36a2cba7cfe090
Jasmine Kwon <jhkwon3@illinois.edu>
2017-12-13 01:24:08 -0600

    hello world v3 test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello World V2
+          Hello World V3
         </p>
       </div>
     );
